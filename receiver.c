4efd8108fde86e69edfe99102bd458e45c5dce3f
root <root@Paulo-PC.localdomain>
2017-10-25 23:13:25 +0000

    Time to expire configured (select())

--- receiver.c
+++ receiver.c
@@ -3,25 +3,38 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<netinet/udp.h>
+#include <arpa/inet.h>
 #include<sys/types.h>
 #include <string.h> /* memset */
 #include <unistd.h> /* close */
+#include<pthread.h>
+#include <time.h>
+#include<errno.h>
 
-#define PORT 8888
+typedef int bool;
+#define true 1
+#define false 0
+#define PORT 67
+#define BUFSIZE 101
+#define SOCKET_ERROR -1
+#define SOCKET_READ_TIMEOUT_SEC 2
+
 void kill(char *msg){
     perror(msg);
     exit(1);
 }
 int main(int argc, char *argv[]){
-    int sock, addr_len;
+    int sock, rv;
     struct sockaddr_in my_address, other_address;
+    char buf[BUFSIZE];
+    socklen_t addr_len = sizeof(struct sockaddr_in);
+    fd_set set;
+    struct timeval timeout;
 
     sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if(sock == -1){
         kill("Socket error!");
     }
-
-    addr_len = sizeof(struct sockaddr_in);
 
     memset((char *)&my_address, 0, addr_len); //zera a memoria
     my_address.sin_family = AF_INET;
@@ -31,10 +44,34 @@ int main(int argc, char *argv[]){
     if(bind(sock, (struct sockaddr*)&my_address, addr_len) == -1){
         kill("Bind error!");
     }
+    memset(buf, 0, BUFSIZE);
+    ssize_t received;
+    printf("Aguardando...\n");
 
-    /*
-    CÓDIGO PARA TRATAR A INFORMAÇÃO RECEBIDA
-    */
+    FD_ZERO(&set); //limpa o set
+    FD_SET(sock, &set); //Une o socket com o set
+    timeout.tv_sec = SOCKET_READ_TIMEOUT_SEC;
+    timeout.tv_usec = 0;
+    bool enable_send = true;
+
+    while(enable_send){
+    rv = select(sock + 1, &set, NULL, NULL, &timeout);
+
+    if (rv == SOCKET_ERROR)
+        kill("Socket error!");
+    else if (rv == 0)
+        printf("Timeout expired!");
+    else{
+        received = recvfrom(sock, buf, BUFSIZE, 0, (struct sockaddr*)&my_address, &addr_len);
+        if (received == SOCKET_ERROR)
+            kill("Error receiving!");
+        else{
+            if(received != BUFSIZE)
+                enable_send = false;
+            printf("Recebido com sucesso! %ld bytes!\n%s\n",received,buf);
+        }
+    }
+}
 
     close(sock);
     return 0;
--- sender.c
+++ sender.c
@@ -7,11 +7,24 @@
 #include<sys/types.h>
 #include <string.h> /* memset */
 #include <unistd.h> /* close */
+#include<pthread.h>
+#include <time.h>
 
+typedef int bool;
+#define true 1
+#define false 0
 #define BUFSIZE 103
-#define PORT 68
+#define PORT 67
 #define FILENAME_SIZE 100
 #define PACKET_SIZE 100
+#define IP_DESTINY "255.255.255.255"
+#define SOCKET_ERROR -1
+#define SOCKET_READ_TIMEOUT_SEC 2
+#define FILENAME "texto.txt"
+
+struct timer{
+    double tempo;
+};
 
 unsigned long packets_to_send;
 
@@ -19,6 +32,7 @@ void kill(char *msg){
     perror(msg);
     exit(1);
 }
+
 unsigned long fsize(char* file)
 {
     FILE * f = fopen(file, "rb");
@@ -34,10 +48,9 @@ int copy_size(unsigned long file_size, int send_qtd){
         return PACKET_SIZE + file_size - (PACKET_SIZE*send_qtd);
 }
 int hasContent(){
-    packets_to_send--;
-
     if(packets_to_send == 0)
         return 0;
+    packets_to_send--;
     return 1;
 }
 unsigned long packets_counter(unsigned long file_size){
@@ -48,10 +61,6 @@ char ack(char atual){
         return '0';
     return '1';
 }
-void enviar(char datagram[]){
-    //enviar com sendto
-    printf("Enviou\n");
-}
 int main(int argc, char *argv[]){
     int sock;
     socklen_t addr_len;
@@ -60,6 +69,7 @@ int main(int argc, char *argv[]){
     struct sockaddr_in my_address, other_address;
     FILE *fd;
     char buffer[BUFSIZE];
+    bool enable_send = true;
 
     sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if(sock == -1)
@@ -70,14 +80,14 @@ int main(int argc, char *argv[]){
     memset((char *)&my_address, 0, addr_len); //zera a memoria
     other_address.sin_family = AF_INET;
     other_address.sin_port = htons(PORT);
-    other_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    other_address.sin_addr.s_addr = inet_addr(IP_DESTINY);
 
+    fd = fopen(FILENAME,"rb");
 
-    fd = fopen("texto.txt","rb");
     if(fd == NULL)
         kill("File not found!");
 
-    file_size = fsize("texto.txt");
+    file_size = fsize(FILENAME);
     packets_to_send = packets_counter(file_size);
 
     char *file = malloc((sizeof(char) * file_size) + 1);
@@ -88,6 +98,19 @@ int main(int argc, char *argv[]){
         kill("Copy error!");
 
     file[file_size] = '\0';
+
+    int rv;
+    fd_set set;
+    struct timeval timeout;
+
+    FD_ZERO(&set); //limpa o set
+    FD_SET(sock, &set); //Une o socket com o set
+    timeout.tv_sec = SOCKET_READ_TIMEOUT_SEC;
+    timeout.tv_usec = 0;
+
+    printf("Iniciando transferencia...\n");
+    int packs_to_expire = 0;
+    ssize_t received;
 
     while(hasContent()){
         size = copy_size(file_size, packets_send + 1);
@@ -100,18 +123,39 @@ int main(int argc, char *argv[]){
 
         start += size;
 
+        while(enable_send && packs_to_expire != 10){
         if(sendto(sock, buffer, (unsigned long) (size + 1), 0, (struct sockaddr*)&other_address, addr_len) == -1)
             kill("Send failed!");
         else
-            printf("Sucessful sent %ld bytes!\n", size+1);
+            printf("%ld bytes enviados com sucesso!\n", size + 1);
 
+        rv = select(sock + 1, &set, NULL, NULL, &timeout);
+
+        if (rv == SOCKET_ERROR)
+            kill("Socket error!");
+        else if (rv == 0){
+            packs_to_expire++;
+            printf("Timeout expired! %d try!\n", packs_to_expire);
+        }
+        else{
+            received = recvfrom(sock, buffer, BUFSIZE, 0, (struct sockaddr*)&my_address, &addr_len);
+            if (received == SOCKET_ERROR)
+                kill("Error receiving!");
+            else{
+                if(buffer[0] == atual_ack){
+                    enable_send = false;
+                    //Ack recebido com sucesso
+                }
+                printf("Ack com sucesso! %ld bytes!\n%s\n",received,buffer);
+            }
+        }
+        }
+        enable_send = true;
         memset(buffer, 0, BUFSIZE);
 
         packets_send++;
     }
-    /*
-        COdigo para tratar informaçao recebida
-    */
+
     if (fclose(fd) != 0)
         kill("Error during file closing!");
 
